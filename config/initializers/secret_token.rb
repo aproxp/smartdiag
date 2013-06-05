112cb39ba7fa3550af9f23d4bf28a64715ec4ab5
aproxp <aproxp@gmail.com>
2013-06-05 16:34:11 +0200

    Fresh new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartdiag::Application.config.secret_token = 'ab53de2a79c23ada8e6717a556c0db7cefa4263174900fcff270152a578f2673ad61b66f55a2450c5ea75307219ae35bf5450a6b6209d8d8745b0ccff791b78b'
