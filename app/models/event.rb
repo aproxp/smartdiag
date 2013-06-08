5122a4a5eecc4d9c977b71530d40753be9101cb6
aproxp <aproxp@gmail.com>
2013-06-08 18:42:23 +0200

    attempted validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,31 @@
 class Event < ActiveRecord::Base
-  attr_accessible :activity_since, :activity_type, :body_part, :description, :drugs, :entered_at, :event_type, :level, :quantity, :temperature, :event_id
+  attr_accessible :activity_since, :activity_type, :body_part, :description, :drugs, :entered_at, :event_type, :level, :quantity, :temperature, :event_id, :user_id
   belongs_to :user
+  validates_presence_of :user, :entered_at
+  validates :event_type, presence: true, inclusion: { in: (0...2) }
+
+  # validates :body_part, :description, :level, presence: true, if:
+  current = Proc.new { |a| a.event_type }
+
+
+
+
+
+
+
+  case current
+
+  when 0
+    validates :body_part, :description, :level, presence: true
+    validates :drugs, :quantity, :temperature, presence: false
+  when 1
+    validates :quantity, :temperature, presence: true
+    validates :drugs, :body_part, :description, :level, presence: false
+  when 2
+    validates :drugs, presence: true
+    validates :body_part, :description, :level, :quantity, :temperature, presence: false
+  end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :bdate, :first_name, :height, :last_name, :weight, :user_id
   has_many :events, dependent: :destroy
+  validates :bdate, :first_name, :height, :last_name, :weight, presence: true
 end
