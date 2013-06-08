39627798ac1fbaafc6361d9640e115aed24b7ce4
hirokiraj <jatczak.j@gmail.com>
2013-06-08 19:38:29 +0200

    with <3

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,31 +1,28 @@
 class Event < ActiveRecord::Base
   attr_accessible :activity_since, :activity_type, :body_part, :description, :drugs, :entered_at, :event_type, :level, :quantity, :temperature, :event_id, :user_id
   belongs_to :user
-  validates_presence_of :user, :entered_at
-  validates :event_type, presence: true, inclusion: { in: (0...2) }
+  validates :user, :entered_at, :event_type, :presence=>true
+  validates :event_type, inclusion: { in: (0...2) }
 
-  # validates :body_part, :description, :level, presence: true, if:
-  current = Proc.new { |a| a.event_type }
+  validates :body_part, :description, :level, :presence=>true, :if=>:is_type_0?
+  validates :drugs, :quantity, :temperature, :length=>{:is=>0, :message=>"must be empty"}, :if=>:is_type_0?
 
+  validates :quantity, :temperature, presence: true, :if=>:is_type_1?
+  validates :drugs, :body_part, :description, :level, :length=>{:is=>0, :message=>"must be empty"}, :if=>:is_type_1?
 
+  validates :drugs, :presence=>true, :if=>:is_type_2?
+  validates :body_part, :description, :level, :quantity, :temperature, :length=>{:is=>0, :message=>"must be empty"}, :if=>:is_type_2?
+  
+  private
+    def is_type_0?
+      self.event_type==0
+    end
 
+    def is_type_1?
+      self.event_type==1
+    end
 
-
-
-
-  case current
-
-  when 0
-    validates :body_part, :description, :level, presence: true
-    validates :drugs, :quantity, :temperature, presence: false
-  when 1
-    validates :quantity, :temperature, presence: true
-    validates :drugs, :body_part, :description, :level, presence: false
-  when 2
-    validates :drugs, presence: true
-    validates :body_part, :description, :level, :quantity, :temperature, presence: false
-  end
-
-
-
+    def is_type_2?
+      self.event_type==2
+    end
 end
