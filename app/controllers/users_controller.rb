8807c2517d4fdda8c987585c947cc7a5084d3351
aproxp <aproxp@gmail.com>
2013-06-11 23:15:37 +0200

    Working high_charts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,23 +14,70 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @time_now = Time.now.utc
+    @time_now = Time.now.utc.at_midnight
 
-    @data = Array.new
+    @event_data = Array.new
+    @activity_data = Array.new
     @user.events.each do |event|
-        if event.event_type == 3
-          date_start = (event.activity_since.utc.to_f*1000.0).to_i
-          date_end = (event.entered_at.utc.to_f*1000.0).to_i
-          @data = @data << [date_start, event.activity_type] << [date_end, event.activity_type]
-        end
+      if event.event_type == 3
+        date_start = (event.activity_since.utc.to_f*1000.0).to_i
+        date_end = (event.entered_at.utc.to_f*1000.0).to_i
+        @activity_data.push({x: date_start, y: event.activity_type, name: ACTIVITY_TYPE[event.activity_type]})
+        @activity_data.push({x: date_end, y: event.activity_type, name: ACTIVITY_TYPE[event.activity_type]})
+      else
+        date = (event.entered_at.utc.to_f*1000.0).to_i
+        @event_data.push({x: date, y: (event.event_type+6), name: EVENT_TYPE[event.event_type]})
+      end
     end
 
 
     @h = LazyHighCharts::HighChart.new('graph') do |f|
-      f.options[:chart][:defaultSeriesType] = 'spline'
-      f.title( text: @user.first_name + " " + @user.last_name + ' ' + 'activity')
+      f.chart( zoomType: 'x')
+      f.title( text: @user.first_name + " " + @user.last_name + ' ' + 'activity and events')
+      f.tooltip(
+        useHTML: true,
+        headerFormat: '<small>{point.key}</small><table>',
+        pointFormat: "<tr><td style=\"color: {series.color}\">{series.name}: </td>" + "<td style=\"text-align: right\"><b>{point.y}</b></td></tr>",
+        crosshairs: true,
+      footerFormat: '</table>')
+
+      f.yAxis(
+        min: 0,
+        max: 11,
+        categories: [nil, "Steady", "Walking", "Biking", "Running", nil, "Pain", "Meal", "Medicine", "Activity Change", "Other"],
+        minorGridLineWidth: 1,
+        gridLineWidth: 1,
+        alternateGridColor: nil,
+        tickInterval: 1,
+        minTickInterval: 1,
+        tickmarkPlacement: 'on',
+        plotBands: [{
+                      from: 0,
+                      to: 5,
+                      color: 'rgba(68, 170, 213, 0.1)',
+                      label: {
+                        text: 'Activity',
+                        style: {
+                          color: '#606060'
+                        }
+                      }
+                    }, {
+                      from: 5,
+                      to: 10,
+                      color: 'rgba(0, 0, 0, 0)',
+                      label: {
+                        text: 'Event',
+                        style: {
+                          color: '#606060'
+                        }
+                      }
+                    }
+                    ]
+      )
       f.xAxis type: 'datetime'
-      f.series(:name=>'Activity', :data=>@data,  pointStart: (Time.now.utc.at_midnight.to_f*1000.0).to_i)
+
+      f.series(type: 'line', :name=>'Activity', :data=>@activity_data,  pointStart: (@time_now.to_f*1000.0).to_i)
+      f.series(type: 'scatter', :name=>'Event', :data=>@event_data,  pointStart: (@time_now.to_f*1000.0).to_i)
     end
 
     respond_to do |format|
